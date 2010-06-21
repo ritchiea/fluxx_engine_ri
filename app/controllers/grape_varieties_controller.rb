311fcfbb0f0527614de080738f8abb5c98e32fb0
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-06-21 13:19:52 -0700

    converted to new DSL

--- app/controllers/grape_varieties_controller.rb
+++ app/controllers/grape_varieties_controller.rb
@@ -1,9 +1,23 @@
 class GrapeVarietiesController < ApplicationController
-  insta_index GrapeVariety, :template => 'grape_variety_list'
-  insta_show GrapeVariety, :template => 'grape_variety_show'
-  insta_new GrapeVariety, :template => 'grape_variety_form'
-  insta_edit GrapeVariety, :template => 'grape_variety_form'
-  insta_post GrapeVariety, :template => 'grape_variety_form'
-  insta_put GrapeVariety, :template => 'grape_variety_form'
-  insta_delete GrapeVariety
+  insta_index GrapeVariety do |insta|
+    insta.template = 'grape_variety_list'
+  end
+  insta_show GrapeVariety do |insta|
+    insta.template = 'grape_variety_show'
+  end
+  insta_new GrapeVariety do |insta|
+    insta.template = 'grape_variety_form'
+  end
+  insta_edit GrapeVariety do |insta|
+    insta.template = 'grape_variety_form'
+  end
+  insta_post GrapeVariety  do |insta|
+    insta.template = 'grape_variety_form'
+  end
+  insta_put GrapeVariety do |insta|
+    insta.template = 'grape_variety_form'
+  end
+  insta_delete GrapeVariety do |insta|
+    insta.template = 'grape_variety_form'
+  end
 end
--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,10 +1,24 @@
 class WinesController < ApplicationController
-  insta_index Wine, :template => 'wine_list'
-  insta_show Wine, :template => 'wine_show'
-  insta_new Wine, :template => 'wine_form'
-  insta_edit Wine, :template => 'wine_form'
-  insta_post Wine, :template => 'wine_form'
-  insta_put Wine, :template => 'wine_form'
-  insta_delete Wine
+  insta_index Wine do |insta|
+    insta.template = 'wine_list'
+  end
+  insta_show Wine do |insta|
+    insta.template = 'wine_show'
+  end
+  insta_new Wine do |insta|
+    insta.template = 'wine_form'
+  end
+  insta_edit Wine do |insta|
+    insta.template = 'wine_form'
+  end
+  insta_post Wine do |insta|
+    insta.template = 'wine_form'
+  end
+  insta_put Wine do |insta|
+    insta.template = 'wine_form'
+  end
+  insta_delete Wine do |insta|
+    insta.template = 'wine_form'
+  end
   
 end
