9bc9b1be0ae3983406e31f90d1886a4108607afc
Casey West <casey@geeknest.com>
2010-06-30 12:22:33 -0400

    Add support for related grape varieties

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -20,5 +20,16 @@ class WinesController < ApplicationController
   insta_delete Wine do |insta|
     insta.template = 'wine_form'
   end
-  
+  insta_related Wine do |insta|
+    insta.add_related do |related|
+      related.display_name = 'Grapes'
+      related.related_class = GrapeVariety
+      related.search_id = :grape_variety
+      related.extra_condition = nil
+      related.max_results = 20
+      related.order = 'name asc'
+      #related.display_template = 'template'
+      related.display_fields = [:name]
+    end
+  end  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -31,10 +31,7 @@ ActiveRecord::Schema.define(:version => 20100615211710) do
   create_table "wines", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name",             :null => false
-    t.integer  "grape_variety_id"
-    t.datetime "locked_until"
-    t.integer  "locked_by_id"
+    t.string   "name",       :null => false
   end
 
 end
